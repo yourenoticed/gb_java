fcaa9be2a0f4e05081290519b9cc40753adaa709
yourenoticed <yourenoticed@gmail.com>
2023-11-29 12:29:23 +0700

    added filtration, still needs to be finished

---
+++ class6/homework/laptop_shop/Laptop.java
@@ -0,0 +1,136 @@
+package laptop_shop;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Laptop {
+    private String brand;
+    private String model;
+    private int priceRUB;
+    private String operatingSystem;
+    private int cpuFrequencyHZ;
+    private int ramMB;
+    private int storageGB;
+    
+    public Laptop(String brand, String model, int priceRUB, String operatingSystem, int cpuFrequencyHZ, int ramMB, int storageGB) {
+        this.brand = brand;
+        this.model = model;
+        this.priceRUB = priceRUB;
+        this.operatingSystem = operatingSystem;
+        this.cpuFrequencyHZ = cpuFrequencyHZ;
+        this.ramMB = ramMB;
+        this.storageGB = storageGB;
+    }
+
+    public boolean isApplicableToFilter(String criterion, List<String> parameters) {
+        switch (criterion) {
+            case "brand":
+                for (String parameter : parameters) {
+                    if (parameter == this.brand) return true;
+                }
+                break;
+
+            case "model":
+                for (String parameter : parameters) {
+                    if (parameter == this.model) return true;
+                }
+                break;
+
+            case "operatingSystem":
+                for (String parameter : parameters) {
+                    if (parameter == this.operatingSystem) return true;
+                }
+                break;
+
+            case "priceRUB":
+                if ((this.priceRUB >= Integer.parseInt(parameters.get(0))) && (this.priceRUB <= Integer.parseInt(parameters.get(1)))) return true; 
+                break;
+
+            case "cpuFrequencyHZ":
+                if ((this.cpuFrequencyHZ >= Integer.parseInt(parameters.get(0))) && (this.cpuFrequencyHZ <= Integer.parseInt(parameters.get(1)))) return true;
+                break;
+
+            case "ramMB":
+                if ((this.ramMB >=Integer.parseInt(parameters.get(0))) && (this.ramMB <= Integer.parseInt(parameters.get(1)))) return true;
+                break;
+
+            case "storageGB":
+                if ((this.storageGB >= Integer.parseInt(parameters.get(0))) && (this.storageGB <= Integer.parseInt(parameters.get(1)))) return true;
+                break;
+
+            default:
+                return false;
+            }
+        return false;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getCpuFrequencyHZ() {
+        return cpuFrequencyHZ;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public int getPriceRUB() {
+        return priceRUB;
+    }
+    
+    public int getRamMB() {
+        return ramMB;
+    }
+
+    public int getStorageGB() {
+        return storageGB;
+    }
+
+    public void setPriceRUB(int price) {
+        this.priceRUB = price;
+    } 
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setCpuFrequencyHZ(int cpuFrequencyHZ) {
+        this.cpuFrequencyHZ = cpuFrequencyHZ;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public void setRamMB(int ramMB) {
+        this.ramMB = ramMB;
+    }
+
+    public void setStorageGB(int storageGB) {
+        this.storageGB = storageGB;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder laptopString = new StringBuilder("Brand: ");
+        laptopString.append(this.brand).append("\n")
+        .append("Model: ").append(this.model).append("\n")
+        .append("Price RUB: ").append(this.priceRUB).append("\n")
+        .append("Operating System: ").append(this.operatingSystem).append("\n")
+        .append("CPU Frequency HZ: ").append(this.cpuFrequencyHZ).append("\n")
+        .append("RAM MB: ").append(this.ramMB).append("\n")
+        .append("Storage GB: ").append(this.storageGB).append("\n");
+
+        return laptopString.toString();
+    }
+}
---
+++ class6/homework/laptop_shop/LaptopShop.java
@@ -0,0 +1,135 @@
+package laptop_shop;
+
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+public class LaptopShop {
+    private Set<Laptop> allLaptops;
+    private Set<Laptop> filteredLaptops;
+    
+    public LaptopShop() {
+        this.allLaptops = new HashSet<>();
+    }
+    
+    public void startFiltration() {
+        this.filteredLaptops = new HashSet<>();
+        this.filteredLaptops.addAll(this.allLaptops);
+    }
+
+    public void applyFilter(String criterion, List<String> parameters) {
+        Set<Laptop> currentFilter = new HashSet<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            if (laptop.isApplicableToFilter(criterion, parameters)) currentFilter.add(laptop);
+        }
+        this.filteredLaptops.retainAll(currentFilter);
+    }
+
+    public void addLaptop(String brand, String model, int priceRUB, String operatingSystem, int cpuFrequencyHZ, int ramMB, int storageGB) {
+        Laptop addedLaptop = new Laptop(brand, model, priceRUB, operatingSystem, cpuFrequencyHZ, ramMB, storageGB);
+        allLaptops.add(addedLaptop);
+    }
+
+    public Laptop deleteLaptop(String brand, String model) {
+        Laptop deletedLaptop = this.findLaptop(brand, model);
+        this.allLaptops.remove(deletedLaptop);
+        return deletedLaptop;
+    }
+
+    public Laptop findLaptop(String brand, String model) {
+        for (Laptop laptop : this.allLaptops) {
+            if ((brand.toLowerCase() == laptop.getBrand().toLowerCase()) && (model.toLowerCase() == laptop.getModel().toLowerCase())) return laptop;
+        }
+        return null;
+    }
+
+    public List<String> getAllBrands() {
+        List<String> allBrands = new ArrayList<>();
+        for (Laptop laptop : this.allLaptops) {
+            allBrands.add(laptop.getBrand());
+        }
+        return allBrands;
+    }
+
+    public List<String> getFilteredBrands() {
+        List<String> filteredBrands = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredBrands.add(laptop.getBrand());
+        }
+        return filteredBrands;
+    }
+
+    public List<String> getAllModels() {
+        List<String> allModels = new ArrayList<>();
+        for (Laptop laptop : this.allLaptops) {
+            allModels.add(laptop.getModel());
+        }
+        return allModels;
+    }
+
+    public List<String> getFilteredModels() {
+        List<String> filteredModels = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredModels.add(laptop.getModels());
+        }
+        return filteredModels;
+    }
+
+    public List<Integer> getFilteredPrices() {
+        List<Integer> filteredPrices = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredPrices.add(laptop.getPriceRUB());
+        }
+        return filteredPrices;
+    }
+
+    public List<String> getAllOperatingSystems() {
+        List<String> allOperatingSystems = new ArrayList<>();
+        for (Laptop laptop : this.allLaptops) {
+            allOperatingSystems.add(laptop.getOperatingSystem());
+        }
+        return allOperatingSystems;
+    }
+
+    public List<String> getFilteredOperatingSystems() {
+        List<String> filteredOS = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredOS.add(laptop.getOperatingSystem());
+        }
+        return filteredOS;
+    }
+
+    public List<Integer> getFilteredCPUFrequencies() {
+        List<Integer> filteredCPUFreq = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredCPUFreq.add(laptop.getCpuFrequencyHZ());
+        }
+        return filteredCPUFreq;
+    }
+
+    public List<Integer> getFilteredRAM() {
+        List<Integer> filteredRAM = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredRAM.add(laptop.getRamMB());
+        }
+        return filteredRAM;
+    }
+
+    public List<Integer> getFilteredStorageMB() {
+        List<Integer> filteredStorages = new ArrayList<>();
+        for (Laptop laptop : this.filteredLaptops) {
+            filteredStorages.add(laptop.getStorageGB());
+        }
+        return filteredStorages;
+    }
+
+    public Set<Laptop> getAllLaptops() {
+        return this.allLaptops;
+    }
+
+    public Set<Laptop> getFilteredLaptops() {
+        return this.filteredLaptops;
+    }
+}
